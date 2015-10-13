acb882b4cfe04d0a8d20150093eb0a7b6d4da831
Sashko Stubailo <sashko@stubailo.com>
2015-10-12 21:14:23 -0700

    Initial example

--- meteor-validating-method-form-example.js
+++ meteor-validating-method-form-example.js
@@ -1,23 +1,55 @@
+const myFormSchema = new SimpleSchema({
+  title: {
+    type: String,
+    label: "Title",
+    max: 200
+  },
+  author: {
+    type: String,
+    label: "Author"
+  },
+  copies: {
+    type: Number,
+    label: "Number of copies",
+    min: 0
+  },
+  lastCheckedOut: {
+    type: Date,
+    label: "Last date this book was checked out",
+    optional: true
+  },
+  summary: {
+    type: String,
+    label: "Brief summary",
+    optional: true,
+    max: 1000
+  }
+});
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  AutoForm.addHooks("special-snowflake", {
+    onError(type, err) {
+      console.log("err", err);
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    myFormSchema() {
+      return myFormSchema;
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Meteor.methods({
+  "my-method"({
+    title,
+    author,
+    copies,
+    lastCheckedOut,
+    summary
+  }) {
+    check(arguments[0], myFormSchema);
+
+    console.log("Method successfully called on server:", arguments[0]);
+  }
+})
